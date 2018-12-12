a70d4de20c80d72b26d273c054dde1f6ac0f025c
IMANOL MUNOZ <rl200156@rlas-macair-29.local>
2018-12-12 11:37:12 -0600

    Initial Commit

---
+++ Operators/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Operators
+//
+//  Created by IMANOL MUNOZ on 9/27/18.
+//  Copyright © 2018 IMANOL MUNOZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
